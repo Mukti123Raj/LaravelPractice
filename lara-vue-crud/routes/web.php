27c962b8e140c581cbcb67fc4ea94bd0df183e38
Mukti Raj Bhusal <muktirajbhusal.cgc@gmail.com>
2025-08-06 18:33:44 +0530

    first Commit

---
+++ lara-vue-crud/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function(){
+    Route::get('/products', [])->name('products.index');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
